083a85493d3b243dfe57adb4a93071b4138a2c50
Kaleb-lister <kaleblister36@gmail.com>
2025-11-30 22:40:50 +0700

    feat: add CRUD to motivation

--- app/schemas/motivation_schema.py
+++ app/schemas/motivation_schema.py
@@ -4,7 +4,7 @@ from typing import Optional
 class MotivationBase(BaseModel):
     quote: str
     uploaderID: Optional[int] = None
-    authorName: Optional[str] = None   # tambahkan ini
+    authorName: Optional[str] = None
 
 class MotivationCreate(MotivationBase):
     pass
@@ -13,4 +13,4 @@ class MotivationResponse(MotivationBase):
     motivationID: int
 
     class Config:
-        orm_mode = True  # untuk membaca dari SQLAlchemy object
+        orm_mode = True
