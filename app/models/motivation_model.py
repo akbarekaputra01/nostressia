3f679ea8d50d3db20432a656388dccb08866947e
Kaleb-lister <kaleblister36@gmail.com>
2026-01-06 23:00:36 +0700

    feat: menambahkan API bookmark

--- app/models/motivation_model.py
+++ app/models/motivation_model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, Text
-
+from sqlalchemy.orm import relationship
 from app.core.database import Base
 
 
@@ -10,3 +10,4 @@ class Motivation(Base):
     quote = Column(Text, nullable=False)
     uploaderID = Column(Integer, nullable=True)
     authorName = Column(Text, nullable=True)
+    bookmarks = relationship("Bookmark", back_populates="motivation", cascade="all, delete-orphan")
